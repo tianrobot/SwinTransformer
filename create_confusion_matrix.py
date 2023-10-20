789598b4741db0a40968817a3806ad5ec2236c9e
tianrobot <103897329+tianrobot@users.noreply.github.com>
2023-10-21 00:43:01 +0200

    Add files via upload

---
+++ create_confusion_matrix.py
@@ -0,0 +1,161 @@
+
+import torch
+import PrettyTable
+import json
+import os 
+import sys
+import argparse
+import tqdm
+
+import albumentations as A
+import numpy as np
+import matplotlib as plt
+
+from albumentations import transforms
+from albumentations import Compose
+from albumentations.pytorch import ToTensorV2
+from my_dataset import read_test_data
+from utils import MyDataSet
+
+from model_v1 import swin_tiny_patch4_window7_224
+
+
+class ConfusionMatrix(object):
+    def __init__(self, num_classes: int, labels: list):
+        self.matrix = np.zeros((num_classes, num_classes))
+        self.num_classes = num_classes
+        self.labels = labels
+
+    def update(self, preds, labels):
+        for p, t in zip(preds, labels):
+            self.matrix[p, t] += 1
+
+    def summary(self):
+        # calculate accuracy
+        sum_TP = 0
+        for i in range(self.num_classes):
+            sum_TP += self.matrix[i, i]
+        acc = sum_TP / np.sum(self.matrix)
+        print("the model accuracy is ", acc)
+
+        # precision, recall, specificity
+        table = PrettyTable()
+        table.field_names = ["", "Precision", "Recall", "Specificity"]
+        for i in range(self.num_classes):
+            TP = self.matrix[i, i]
+            FP = np.sum(self.matrix[i, :]) - TP
+            FN = np.sum(self.matrix[:, i]) - TP
+            TN = np.sum(self.matrix) - TP - FP - FN
+            Precision = round(TP / (TP + FP), 3) if TP + FP != 0 else 0.
+            Recall = round(TP / (TP + FN), 3) if TP + FN != 0 else 0.
+            Specificity = round(TN / (TN + FP), 3) if TN + FP != 0 else 0.
+            table.add_row([self.labels[i], Precision, Recall, Specificity])
+        print(table)
+
+    def plot(self):
+        matrix = self.matrix
+        print(matrix)
+        plt.imshow(matrix, cmap=plt.cm.Blues)
+
+        # 设置x轴坐标label
+        plt.xticks(range(self.num_classes), self.labels, rotation=45)
+        # 设置y轴坐标label
+        plt.yticks(range(self.num_classes), self.labels)
+        # 显示colorbar
+        plt.colorbar()
+        plt.xlabel('True Labels')
+        plt.ylabel('Predicted Labels')
+        plt.title('Confusion matrix')
+
+        # 在图中标注数量/概率信息
+        thresh = matrix.max() / 2
+        for x in range(self.num_classes):
+            for y in range(self.num_classes):
+                # 注意这里的matrix[y, x]不是matrix[x, y]
+                info = int(matrix[y, x])
+                plt.text(x, y, info,
+                         verticalalignment='center',
+                         horizontalalignment='center',
+                         color="white" if info > thresh else "black")
+        plt.tight_layout()
+        plt.show()
+
+
+def main(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    print(f"using device: {device}")
+
+    val_images_path, val_images_label = read_test_data(args.data_path)
+
+    data_transform = {
+        Compose([A.Resize(args.img_size,args.img_size),
+                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                 ToTensorV2(),
+                 ])}
+
+    # 实例化验证数据集
+    val_dataset = MyDataSet(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform)
+
+    nw = min([os.cpu_count(), args.batch_size if args.batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=args.batch_size,
+                                             shuffle=False,
+                                             pin_memory=True,
+                                             num_workers=nw,
+                                             collate_fn=val_dataset.collate_fn)
+
+    # model = swin_tiny_patch4_window7_224(num_classes=args.num_classes, img_size=args.img_size).to(device)
+    # model = swin_base_patch4_window7_224_in22k(num_classes=args.num_classes, img_size=args.img_size).to(device)
+    # model = resnet101(num_classes=args.num_classes).to(device)
+    model = swin_tiny_patch4_window7_224(num_classes=args.num_classes, img_size=args.img_size).to(device)
+
+    # load pretrain weights
+    assert os.path.exists(args.weights), "cannot find {} file".format(args.weights)
+    model.load_state_dict(torch.load(args.weights, map_location=device))
+    model.to(device)
+
+    # read class_indict
+    json_label_path = './class_indices.json'
+    assert os.path.exists(json_label_path), "cannot find {} file".format(json_label_path)
+    json_file = open(json_label_path, 'r')
+    class_indict = json.load(json_file)
+
+    labels = [label for _, label in class_indict.items()]
+    confusion = ConfusionMatrix(num_classes=args.num_classes, labels=labels)
+    model.eval()
+    with torch.no_grad():
+        for val_data in tqdm(val_loader, file=sys.stdout):
+            val_images, val_labels = val_data
+            outputs = model(val_images.to(device))
+            outputs = torch.softmax(outputs, dim=1)
+            outputs = torch.argmax(outputs, dim=1)
+            confusion.update(outputs.to("cpu").numpy(), val_labels.to("cpu").numpy())
+    confusion.plot()
+    confusion.summary()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f')
+    parser.add_argument('--num_classes', type=int, default=4)
+    parser.add_argument('--batch-size', type=int, default=32)
+    parser.add_argument('--img_size', type=int, default=224)
+
+    # dataset
+    # http://download.tensorflow.org/example_images/flower_photos.tgz
+    parser.add_argument('--data-path', type=str,
+                        default="dataset/CT/test")
+
+    # Training weight paths
+    parser.add_argument('--weights', type=str, default='/content/drive/MyDrive/test_model/weights/Resnet101-7_ct.pth',
+                        help='initial weights path')
+    # Whether to freeze weights
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    opt = parser.parse_args()
+
+    main(opt)
